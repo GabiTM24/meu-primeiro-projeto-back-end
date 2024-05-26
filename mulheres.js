227e291af13c2e3aabedd6cf030f087b2730f088
GabiTM24 <gabrielaadm2017@gmail.com>
2024-05-26 08:41:31 -0300

    insere a nova versao do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,90 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //aqui estou iniciando o express
+const router = express.Router() // aqui estou configurando a primeira parte da rota
+const cors = require('cors') //aqui estou trazendo o pacote cors que permite consumir esta API no Front-end
+const conectaBancoDeDados = require('./bancoDeDados') //aqui estou ligando ao arquivo bancoDeDados
+conectaBancoDeDados() // estou chamando a função que conecta o banco de dados
 
-const app = express()
-const porta = 3333
+const Mulher = require('./mulherModel')
 
-const mulheres = [
-    {
-    nome:'Gabriela Melo',
-    imagem:'https://media.licdn.com/dms/image/D4D03AQE7DsoUe-X7xg/profile-displayphoto-shrink_800_800/0/1691062331804?e=1721260800&v=beta&t=2VB4TvAHGoeAKMtf33qdnfS-6SQm0EMdkbM8WtfhgM0',
-    minibio:'Especialista em Projetos e Inovação'
-    },
-    {
-    nome:'Lucimara Melo',
-    imagem:'https://media.licdn.com/dms/image/D4D16AQG93xqCxxM4Hg/profile-displaybackgroundimage-shrink_350_1400/0/1684098139004?e=1721260800&v=beta&t=rvJLATw6ps33jwmBuY2PijkW2GW3FxiEXzXRrmmavP8',
-    minibio:'Tecnólogo em Gestão Ambiental'
-    },
-    {
-    nome:'Letícia Marielle',
-    imagem:'https://media.licdn.com/dms/image/D4E16AQGs5gjsFnb80Q/profile-displaybackgroundimage-shrink_350_1400/0/1710356004985?e=1721260800&v=beta&t=C1Zp585dpOIHw24Jufz1j7CNOG9upD0gDAiUlPfUpN0',
-    minibio:'Scrum Master' 
+const app = express() // aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
+const porta = 3333 // aqui estou criando a porta
+
+//GET
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    } catch (erro){
+        console.log()
     }
-]
-
-function mostraMulheres(request, response) {
-response.json (mulheres)
 }
 
+//POST
+async function criaMulher(request,response){
+    const novaMulher = new Mulher({
+        nome:request.body.nome,
+        imagem:request.body.imagem,
+        minibio:request.body.minibio,
+        citacao:request.body.citacao
+    })
+
+    try{
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    }catch(erro){
+        console.log(erro)
+    }
+}
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres',mostraMulheres))
-app.listen(porta, mostraPorta)
+//PATCH
+async function corrigeMulher(request,response){
+    try{
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+    
+        if (request.body.nome) {
+        mulherEncontrada.nome = request.body.nome
+    }
+
+    if (request.body.minibio) {
+        mulherEncontrada.minibio = request.body.minibio
+    }
+
+    if (request.body.imagem) {
+        mulherEncontrada = request.body.imagem
+    }
+
+    if (request.body.citacao) {
+        mulherEncontrada = request.body.citacao
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+    response.json(mulherAtualizadaNoBancoDeDados)
+} catch (erro){
+        console.log(erro)
+ }
+}
+
+//DELETE
+async function deletaMulher(request, response){
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem:'Mulher deletada com sucesso'})
+    } catch(erro){
+        console.log(erro)
+    }
+}   
+
+app.use(router.get('/mulheres',mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) // configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH /mulheres/:id
+app.use(router.delete('/mulheres/:id', deletaMulher)) // configurei rota DELETE /mulheres
+app.listen(porta, mostraPorta) // servidor ouvindo a porta
